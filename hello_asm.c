632d81f6c2aaa4e26600b4704583cb6cdddd2d9c
Yan Li <eyeplum@gmail.com>
2015-08-03 20:33:50 +0800

    Initial commit

---
+++ hello_asm.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char * args[])
+{
+  for (int i = 0; i < 10; ++i)
+  {
+    int a = i;    
+  }
+
+  return 0;
+}
---
+++ hello_asm.s
@@ -0,0 +1,38 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.macosx_version_min 10, 11
+	.globl	_main
+	.align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## BB#0:
+	push	rbp
+Ltmp0:
+	.cfi_def_cfa_offset 16
+Ltmp1:
+	.cfi_offset rbp, -16
+	mov	rbp, rsp
+Ltmp2:
+	.cfi_def_cfa_register rbp
+	mov	dword ptr [rbp - 4], 0
+	mov	dword ptr [rbp - 8], edi
+	mov	qword ptr [rbp - 16], rsi
+	mov	dword ptr [rbp - 20], 0
+LBB0_1:                                 ## =>This Inner Loop Header: Depth=1
+	cmp	dword ptr [rbp - 20], 10
+	jge	LBB0_4
+## BB#2:                                ##   in Loop: Header=BB0_1 Depth=1
+	mov	eax, dword ptr [rbp - 20]
+	mov	dword ptr [rbp - 24], eax
+## BB#3:                                ##   in Loop: Header=BB0_1 Depth=1
+	mov	eax, dword ptr [rbp - 20]
+	add	eax, 1
+	mov	dword ptr [rbp - 20], eax
+	jmp	LBB0_1
+LBB0_4:
+	xor	eax, eax
+	pop	rbp
+	ret
+	.cfi_endproc
+
+
+.subsections_via_symbols
